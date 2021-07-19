467fe333cd5ec0dc044ab17cb50f6487010035d7
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-07-19 16:55:06 -0600

    Add test; passing for merchant show.

---
+++ spec/requests/api/v1/merchants/show_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+RSpec.describe 'Merchants API' do
+  before :each do
+    FactoryBot.reload
+  end
+
+  describe 'Happy Path' do
+    it 'returns one merchant by id' do
+      create_list(:merchant, 10)
+
+      get '/api/v1/merchants/7'
+      expect(response).to be_successful
+      merchant = JSON.parse(response.body, symbolize_names: true)
+      expect(merchant.count).to eq(1)
+      end
+  end
+
+  describe 'Sad Path' do
+    it 'returns 404 with invalid id' do
+    end
+  end
+end
