61bb657d6d8ff39e07dacc42e609add5d58de5b6
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-07-19 16:20:40 -0600

    Add test; conditional for per_page high count.

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-    merchants = Merchant.pagination(params.fetch(:per_page, 20).to_i, params.fetch(:page, 1).to_i)
+    if (params[:per_page]).to_i > 50
+      merchants = Merchant.all
+    else
+      merchants = Merchant.pagination(params.fetch(:per_page, 20).to_i, params.fetch(:page, 1).to_i)
+    end
     render json: MerchantSerializer.new(merchants)
   end
 end
--- spec/requests/api/v1/merchants/index_spec.rb
+++ spec/requests/api/v1/merchants/index_spec.rb
@@ -40,6 +40,16 @@ RSpec.describe 'Merchants API' do
       expect(response).to be_successful
       merchants = JSON.parse(response.body, symbolize_names: true)
       expect(merchants[:data].count).to eq(20)
+
+    end
+
+    it 'fetch all merchants if per_page count is high' do
+      create_list(:merchant, 100)
+
+      get '/api/v1/merchants?per_page=51'
+      expect(response).to be_successful
+      merchants = JSON.parse(response.body, symbolize_names: true)
+      expect(merchants[:data].count).to eq(100)
     end
   end
 
