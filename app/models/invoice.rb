be50a24bd54700f809ea770489ee89d2dc229dbc
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-07-19 15:38:32 -0600

    Add models and model specs; passing.

---
+++ app/models/invoice.rb
@@ -0,0 +1,7 @@
+class Invoice < ApplicationRecord
+  belongs_to :customer
+  belongs_to :merchant
+  has_many :transactions
+  has_many :invoice_items
+  validates_presence_of :customer_id, :merchant_id, :status
+end
