548c0e73cbf90e3ae5823dff7b33ce8bb324a35a
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-07-20 13:34:40 -0600

    Add create item spec.

---
+++ spec/requests/api/v1/items/create_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+RSpec.describe 'Items New API' do
+  before :each do
+    FactoryBot.reload
+  end
+
+  describe 'Happy Path' do
+    it 'can create new item' do
+      merchant = create(:merchant)
+      items = create(:item)
+      expect(Item.count).to eq(1)
+
+      item_params = {
+        name: 'New Item',
+        description: 'Shiny & New',
+        unit_price: 1.99,
+        merchant_id: merchant.id
+      }
+
+      post '/api/v1/items', params: item_params
+      expect(Item.count).to eq(2)
+      #what to expect for status?
+    end
+
+    # it 'can delete item' do
+    # end
+  end
+
+  # describe 'Sad Path' do
+  #   it '' do
+  #   end
+  # end
+end
