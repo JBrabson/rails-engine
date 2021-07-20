b6d767abcdc2770f593230fa5394a495284350db
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-07-20 12:45:30 -0600

    Add to items index.

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-      items = Item.all
+      items = Item.pagination(params.fetch(:per_page, 20).to_i, params.fetch(:page, 1).to_i)
       render json: ItemSerializer.new(items)
   end
 end
--- spec/requests/api/v1/items/index_spec.rb
+++ spec/requests/api/v1/items/index_spec.rb
@@ -5,18 +5,34 @@ RSpec.describe 'Items API' do
   end
 
   describe 'Happy Path' do
-    it 'returns list of all items' do
+    it 'returns list of all items, 20 per page' do
       create_list(:merchant, 7)
-      create_list(:item, 50)
+      create_list(:item, 36)
 
       get '/api/v1/items'
-
+      items_pg1 = JSON.parse(response.body, symbolize_names: true)
       expect(response).to be_successful
+      expect(items_pg1[:data].count).to eq(20)
 
-      items = JSON.parse(response.body, symbolize_names: true)
+      items_pg1[:data].each do |item|
+        expect(item).to have_key(:id)
+        expect(item[:id]).to be_a(String)
+        expect(item[:attributes]).to have_key(:name)
+        expect(item[:attributes][:name]).to be_a(String)
+        expect(item[:attributes]).to have_key(:description)
+        expect(item[:attributes][:description]).to be_a(String)
+        expect(item[:attributes]).to have_key(:unit_price)
+        expect(item[:attributes][:unit_price]).to be_a(Float)
+        expect(item[:attributes]).to have_key(:merchant_id)
+        expect(item[:attributes][:merchant_id]).to be_an(Integer)
+      end
 
-      expect(items[:data].count).to eq(50)
-      items[:data].each do |item|
+      get '/api/v1/items?page=2'
+      items_pg2 = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to be_successful
+      expect(items_pg2[:data].count).to eq(16)
+
+      items_pg2[:data].each do |item|
         expect(item).to have_key(:id)
         expect(item[:id]).to be_a(String)
         expect(item[:attributes]).to have_key(:name)
@@ -29,21 +45,46 @@ RSpec.describe 'Items API' do
         expect(item[:attributes][:merchant_id]).to be_an(Integer)
       end
     end
+
+    it 'first page of 20 matches first 20 in database' do
+      create_list(:merchant, 2)
+      create_list(:item, 30)
+
+      get '/api/v1/items?page=1'
+      expect(response).to be_successful
+      items = JSON.parse(response.body, symbolize_names: true)
+      expect(items[:data].count).to eq(20)
+      expect(items[:data].first[:id]).to eq("1")
+      expect(items[:data].last[:id]).to eq("20")
+    end
+
+    it 'second page of 20 matches 21-40 in database' do
+      create_list(:merchant, 2)
+      create_list(:item, 45)
+
+      get '/api/v1/items?page=2'
+      expect(response).to be_successful
+      items = JSON.parse(response.body, symbolize_names: true)
+      expect(items[:data].count).to eq(20)
+      expect(items[:data].first[:id]).to eq("21")
+      expect(items[:data].last[:id]).to eq("40")
+    end
   end
 
-  # describe 'Sad Path' do
-  #   it 'fetching page 1 if page is 0 or lower' do
-  #     create_list(:merchant, 50)
-  #
-  #     get '/api/v1/merchants?page=1'
-  #     page1 = JSON.parse(response.body, symbolize_names: true)
-  #
-  #     get '/api/v1/merchants?page=0'
-  #     page0 = JSON.parse(response.body, symbolize_names: true)
-  #     expect(response).to be_successful
-  #     expect(page1[:data].count).to eq(20)
-  #
-  #     expect(page1).to eq(page0)
-  #   end
-  # end
+  describe 'Sad Path' do
+    it 'fetching page 1 if page is 0 or lower' do
+      create_list(:merchant, 2)
+      create_list(:item, 30)
+
+      get '/api/v1/items?page=1'
+      page1 = JSON.parse(response.body, symbolize_names: true)
+
+      get '/api/v1/items?page=0'
+      page0 = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to be_successful
+      expect(page1[:data].count).to eq(20)
+
+      expect(page1).to eq(page0)
+    end
+  end
 end
