3a008f629b60bb31f7db23f691209ffcf496b627
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-07-19 18:20:16 -0600

    Add namespace for merchant's items.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get '/:id/items', to: 'items#index'
       resources :merchants, only: [:index, :show]
+      end
     end
   end
 end
