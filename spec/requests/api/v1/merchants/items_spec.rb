9fd7b32fd7ff065481c71fe68135f40b664230c2
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-07-19 18:20:48 -0600

    Add happy path for merchant's items.

---
+++ spec/requests/api/v1/merchants/items_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+RSpec.describe 'Merchants API' do
+  before :each do
+    FactoryBot.reload
+    merchant = create(:merchant)
+    items = create_list(:item, 7, merchant: merchant)
+    get "/api/v1/merchants/1/items"
+  end
+
+  describe 'Merchant Items Index Page' do
+    it "returns list of merchant's items" do
+      expect(response).to be_successful
+      items = JSON.parse(response.body, symbolize_names: true)
+      expect(items[:data].class).to eq(Array)
+      expect(items[:data].count).to eq(7)
+      items[:data].each do |item|
+        expect(item[:attributes]).to have_key(:name)
+        expect(item[:attributes][:name]).to be_a(String)
+        expect(item[:attributes]).to have_key(:description)
+        expect(item[:attributes][:description]).to be_a(String)
+        expect(item[:attributes]).to have_key(:unit_price)
+        expect(item[:attributes][:unit_price]).to be_a(Float)
+        expect(item[:attributes]).to have_key(:merchant_id)
+        expect(item[:attributes][:merchant_id]).to be_an(Integer)
+      end
+    end
+  end
+
+  # describe 'Sad Path' do
+  #   it '' do
+  #   end
+  # end
+end
