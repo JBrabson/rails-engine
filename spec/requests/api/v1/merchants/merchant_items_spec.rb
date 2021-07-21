2b9ed20233d3f61f2c6ba0a4c4769a53f94f7973
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-07-20 20:44:13 -0600

    Add test for relationship endpoints: merchant items.

--- spec/requests/api/v1/merchants/merchant_items_spec.rb
+++ spec/requests/api/v1/merchants/merchant_items_spec.rb
@@ -7,23 +7,23 @@ RSpec.describe 'Merchant Items Index API' do
   describe 'Happy Path' do
     it 'returns all items that belong to merchant' do
       merchant = create(:merchant)
-      items = create_list(:item, 7)
+      items = create_list(:item, 7, merchant: merchant)
 
-      get '/api/v1/merchants/1/items'
+      get "/api/v1/merchants/#{merchant.id}/items"
       expect(response).to be_successful
       items = merchant.items
       items_json = JSON.parse(response.body, symbolize_names: true)
       expect(items.count).to eq(7)
-      expect(items_json[:data].first[:id]).to eq(items.first.id.to_s)
+      expect(items_json[:data].first[:id].to_s).to eq(items.first.id.to_s)
     end
   end
 
-  describe 'Sad Path' do
-    it 'returns 404 with invalid id' do
-      create_list(:merchant, 10)
-
-      get '/api/v1/merchants/11'
-      expect(response).to have_http_status(404)
-    end
-  end
+  # describe 'Sad Path' do
+  #   it 'returns 404 with invalid id' do
+  #     create_list(:merchant, 10)
+  #
+  #     get '/api/v1/merchants/11'
+  #     expect(response).to have_http_status(404)
+  #   end
+  # end
 end
